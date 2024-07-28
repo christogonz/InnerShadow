038f79640a4cb45da9a80b9929fbf9a63900e467
Chris Gonzalez <christogonz92@gmail.com>
2024-07-28 11:09:11 +0200

    Initial Commit

---
+++ InnerShadow/InnerShadowApp.swift
@@ -0,0 +1,17 @@
+//
+//  InnerShadowApp.swift
+//  InnerShadow
+//
+//  Created by Christopher Gonzalez on 2024-07-28.
+//
+
+import SwiftUI
+
+@main
+struct InnerShadowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
